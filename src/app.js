bb7035bbc171cbeb8b1a49c405adb3fc79e5299f
June <juneapps12@gmail.com>
2019-10-28 13:02:07 +0100

    completed module 2

--- src/app.js
+++ src/app.js
@@ -10,7 +10,37 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', {title:'index'}));
+const accountData = fs.readFileSync(
+  path.join(__dirname, 'json', 'accounts.json'), 'utf-8'
+);
+
+const accounts = JSON.parse(accountData);
+
+app.get('/', (req, res) => res.render('index', {title:'Account Summary', accounts}));
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {user:users[0]});
+})
+
+app.get('/savings', (req, res) => {
+  res.render('account', {account: accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', {account: accounts.checking})
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', {account: accounts.credit})
+});
+app.get('/transfer', (req, res) => {
+  res.render('transfer' )
+})
+const userData = fs.readFileSync(
+  path.join(__dirname, 'json', 'users.json'), 'utf-8'
+);
+
+const users = JSON.parse(userData);
 
 
 app.listen(3000, ()=> console.log('Ps project running on port 3000'));
